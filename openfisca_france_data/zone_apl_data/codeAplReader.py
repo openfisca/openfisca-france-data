1112dbd35a15a2ca6d33e9c4ad7606da30ebb343
Emmanuel Raviart <emmanuel@raviart.com>
2015-01-02 08:31:54 +0100

    Update obsolete copyright headers.

--- openfisca_france_data/zone_apl_data/codeAplReader.py
+++ openfisca_france_data/zone_apl_data/codeAplReader.py
@@ -1,26 +1,27 @@
-# -*- coding:utf-8 -*-
-# Copyright © 2011 Clément Schaff, Mahdi Ben Jelloul
-
-"""
-µSim, Logiciel libre de simulation du système socio-fiscal français
-Copyright © 2011 Clément Schaff, Mahdi Ben Jelloul
+# -*- coding: utf-8 -*-
 
 
-This file is part of µSim.
+# OpenFisca -- A versatile microsimulation software
+# By: OpenFisca Team <contact@openfisca.fr>
+#
+# Copyright (C) 2011, 2012, 2013, 2014, 2015 OpenFisca Team
+# https://github.com/openfisca
+#
+# This file is part of OpenFisca.
+#
+# OpenFisca is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# OpenFisca is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    µSim is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    µSim is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with µSim.  If not, see <http://www.gnu.org/licenses/>.
-"""
 
 import csv, pickle
 
@@ -28,14 +29,14 @@ if __name__ == '__main__':
 
     codeDict = {}
     fileName = 'zone_apl.csv'
-    
+
     code = csv.reader(open(fileName), delimiter = ";")
-    
+
     for row in code:
         codeDict.update({row[2]:(row[1],row[4])})
-    
+
     #print codeDict['75017']
-    
+
     outputFile = open("code_apl", 'wb')
     pickle.dump(codeDict, outputFile)
     outputFile.close()
