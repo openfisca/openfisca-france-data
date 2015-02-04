3cf6709b12fdf34c876e3ffde8498b9f2adf5028
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2015-02-04 13:14:15 +0100

    Mutualize config_directory

--- openfisca_france_data/input_data_builders/build_openfisca_indirect_taxation_survey_data/step_03_calage.py
+++ openfisca_france_data/input_data_builders/build_openfisca_indirect_taxation_survey_data/step_03_calage.py
@@ -26,23 +26,23 @@
 
 import os
 import logging
-import pkg_resources
+
+from ConfigParser import SafeConfigParser
 
 import pandas as pd
 from pandas import read_excel, merge
 
 log = logging.getLogger(__name__)
 
-from ConfigParser import SafeConfigParser
+
+from openfisca_france_data import config_files_directory
 
 def get_CN_data_frames(year = None,year_calage=None):
     '''
-    
+
     '''
     assert year is not None
     parser = SafeConfigParser()
-    openfisca_france_data_location = pkg_resources.get_distribution('openfisca-france-data').location
-    config_files_directory = os.path.join(openfisca_france_data_location)
     config_local_ini = os.path.join(config_files_directory, 'config_local.ini')
     config_ini = os.path.join(config_files_directory, 'config.ini')
     parser.read([config_ini, config_local_ini])
@@ -60,18 +60,18 @@ def get_CN_data_frames(year = None,year_calage=None):
 
     selected_conso_cn_data_frame = selected_conso_cn_data_frame[selected_conso_cn_data_frame['len_code']==6]
     selected_conso_cn_data_frame = selected_conso_cn_data_frame.drop(['len_code','code_unicode'],1)
-   
+
     if year_calage <> year:
         selected_conso_cn_data_frame.rename(columns={
-            year: 'consoCN_COICOP_{}'.format(year), 
+            year: 'consoCN_COICOP_{}'.format(year),
             year_calage: 'consoCN_COICOP_{}'.format(year_calage),
             'Code': 'poste'}, inplace=True)
     else:
         selected_conso_cn_data_frame.rename(columns={
-            year: 'consoCN_COICOP_{}'.format(year), 
-            'Code': 'poste'}, inplace=True)        
+            year: 'consoCN_COICOP_{}'.format(year),
+            'Code': 'poste'}, inplace=True)
     return selected_conso_cn_data_frame
-    
+
 
 def wsum(groupe, var):
     '''
@@ -80,7 +80,7 @@ def wsum(groupe, var):
     d = groupe[var]
     w = groupe['pondmen']
     return (d * w).sum()
-    
+
 def collapsesum(dataframe, groupe, var):
     '''
     Pour une variable, fonction qui calcule la moyenne pondérée au sein de chaque groupe.
@@ -88,11 +88,11 @@ def collapsesum(dataframe, groupe, var):
     grouped = dataframe.groupby([groupe])
     var_weighted_grouped = grouped.apply(lambda x: wsum(groupe = x,var =var))
     return var_weighted_grouped
-    
+
 # TODO: finir cette fonction qui sélectionne la data d'input automatiquement en fonction de l'année de simulation (calage)
 #def define_year_data(year_data_list, year_calage):
 #    diff=[]
-#    for i in range(0,len(year_data_list)-1):    
+#    for i in range(0,len(year_data_list)-1):
 #        print year_data_list[1]
 ##        diff[i] = year_calage - year_data_list[i]
 #    return diff
@@ -109,7 +109,7 @@ if __name__ == '__main__':
     year_calage=2010
 
     get_CN_data_frames(year=year, year_calage=year_calage)
-    
+
     #TODO: get depense_data_frame from step_O1
     df= pd.pandas.io.stata.read_stata("/Volumes/LE GROS DD/IPP/TaxationIndirecte/Donnees_BdF_homogeneisees/2005/dépenses_BdF.dta")
 #    df= pd.pandas.io.stata.read_stata("C:/Users/Vincent/Desktop/BdF/Taxation indirecte/Donnees BdF homogeneisees/2005/depenses_BdF.dta")
@@ -124,5 +124,5 @@ if __name__ == '__main__':
     depenses_cn_df['ratio_cn{}_cn{}'.format(year, year_calage)] = depenses_cn_df['consoCN_COICOP_{}'.format(year_calage)]/depenses_cn_df['consoCN_COICOP_{}'.format(year)]
     depenses_cn_df['ratio_bdf{}_cn{}'.format(year,year)] = 1000000 * depenses_cn_df['consoCN_COICOP_{}'.format(year)]/depenses_cn_df['conso_bdf{}'.format(year)]
     print depenses_cn_df
-    
+
     log.info("step 03 calage duration is {}".format(time.clock() - deb))
