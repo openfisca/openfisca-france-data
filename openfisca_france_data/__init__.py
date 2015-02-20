5af0b47606ef2dd1eda78d30172409f7ad3e6a9e
Rossi <rossi.a.rafeh@riskergy.com>
2015-02-20 10:26:37 +0100

    Added a clarification for the harmonization function. Is removing c14 warranted?

--- openfisca_france_data/__init__.py
+++ openfisca_france_data/__init__.py
@@ -25,85 +25,85 @@
 
 import os
 import pkg_resources
-
-from openfisca_core import reforms
-import openfisca_france
-
-
-ReferenceTaxBenefitSystem = openfisca_france.init_country()
-reference_tax_benefit_system = ReferenceTaxBenefitSystem()
-SurveyTaxBenefitSystem = reforms.new_simple_reform_class(
-    name = u"OpenFisca for survey data",
-    reference = reference_tax_benefit_system,
-    )
-
-from .model import input_variables  # Load input variables into entities. # noqa analysis:ignore
-from .model import model  # Load output variables into entities. # noqa analysis:ignore
-
-
-def init_country():
-    """Create a country-specific TaxBenefitSystem for use with data."""
-    return SurveyTaxBenefitSystem
-
-
-AGGREGATES_DEFAULT_VARS = [
-    'cotsoc_noncontrib',
-    'csg',
-    'crds',
-    'irpp',
-    'ppe',
-    'ppe_brute',
-    'af',
-    'af_base',
-    'af_majo',
-    'af_forf',
-    'cf',
-    'paje_base',
-    'paje_nais',
-    'paje_clca',
-    'paje_clmg',
-    'ars',
-    'aeeh',
-    'asf',
-    'aspa',
-    'aah',
-    'caah',
-    'rsa',
-    'rsa_act',
-    'aefa',
-    'api',
-#    'majo_rsa',
-    'psa',
-    'aides_logement',
-    'alf',
-    'als',
-    'apl',
-    ]
-    # ajouter csgd pour le calcul des agrégats erfs
-    # ajouter rmi pour le calcul des agrégats erfs
-
+#
+#from openfisca_core import reforms
+#import openfisca_france
+#
+#
+#ReferenceTaxBenefitSystem = openfisca_france.init_country()
+#reference_tax_benefit_system = ReferenceTaxBenefitSystem()
+#SurveyTaxBenefitSystem = reforms.new_simple_reform_class(
+#    name = u"OpenFisca for survey data",
+#    reference = reference_tax_benefit_system,
+#    )
+#
+#from .model import input_variables  # Load input variables into entities. # noqa analysis:ignore
+#from .model import model  # Load output variables into entities. # noqa analysis:ignore
+#
+#
+#def init_country():
+#    """Create a country-specific TaxBenefitSystem for use with data."""
+#    return SurveyTaxBenefitSystem
+#
+#
+#AGGREGATES_DEFAULT_VARS = [
+#    'cotsoc_noncontrib',
+#    'csg',
+#    'crds',
+#    'irpp',
+#    'ppe',
+#    'ppe_brute',
+#    'af',
+#    'af_base',
+#    'af_majo',
+#    'af_forf',
+#    'cf',
+#    'paje_base',
+#    'paje_nais',
+#    'paje_clca',
+#    'paje_clmg',
+#    'ars',
+#    'aeeh',
+#    'asf',
+#    'aspa',
+#    'aah',
+#    'caah',
+#    'rsa',
+#    'rsa_act',
+#    'aefa',
+#    'api',
+##    'majo_rsa',
+#    'psa',
+#    'aides_logement',
+#    'alf',
+#    'als',
+#    'apl',
+#    ]
+#    # ajouter csgd pour le calcul des agrégats erfs
+#    # ajouter rmi pour le calcul des agrégats erfs
+#
 openfisca_france_location = pkg_resources.get_distribution('openfisca-france-data').location
 default_config_files_directory = os.path.join(openfisca_france_location)
-
-COUNTRY_DIR = os.path.dirname(os.path.abspath(__file__))
-FILTERING_VARS = ["champm"]
-PLUGINS_DIR = os.path.join(COUNTRY_DIR, 'plugins')
-WEIGHT = "wprm"
-WEIGHT_INI = "wprm_init"
-
-
-def preproc_inputs(self, datatable):
-    """Preprocess inputs table: country specific manipulations
-
-    Parameters
-    ----------
-    datatable : a DataTable object
-                the DataTable containing the input variables of the model
-
-    """
-    try:
-        datatable.propagate_to_members(WEIGHT, 'ind')
-    #    datatable.propagate_to_members('rfr_n_2', 'ind')
-    #    datatable.propagate_to_members('nbptr_n_2', 'ind')
-    except:
-        pass
+#
+#COUNTRY_DIR = os.path.dirname(os.path.abspath(__file__))
+#FILTERING_VARS = ["champm"]
+#PLUGINS_DIR = os.path.join(COUNTRY_DIR, 'plugins')
+#WEIGHT = "wprm"
+#WEIGHT_INI = "wprm_init"
+#
+#
+#def preproc_inputs(self, datatable):
+#    """Preprocess inputs table: country specific manipulations
+#
+#    Parameters
+#    ----------
+#    datatable : a DataTable object
+#                the DataTable containing the input variables of the model
+#
+#    """
+#    try:
+#        datatable.propagate_to_members(WEIGHT, 'ind')
+#    #    datatable.propagate_to_members('rfr_n_2', 'ind')
+#    #    datatable.propagate_to_members('nbptr_n_2', 'ind')
+#    except:
+#        pass
