6ef57118ce6eee99ab1e847b5bf39af4e7a814b1
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2016-01-15 16:24:43 +0100

    Fix merge error Improve .gitignore

--- openfisca_france_data/model/common.py
+++ openfisca_france_data/model/common.py
@@ -194,10 +194,7 @@ def mark_weighted_percentiles(a, labels, weights, method, return_quantiles=False
 #            )
 #        )
 
-<<<<<<< HEAD
 
-=======
->>>>>>> openfisca/master
 class champm_individus(Variable):
     column = BoolCol
     entity_class = Individus
