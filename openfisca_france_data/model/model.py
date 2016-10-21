78cf2a6762013ceb22885973ffd022924ab15a0e
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2016-10-21 16:56:17 +0200

    Remove obsolete import

--- openfisca_france_data/model/model.py
+++ openfisca_france_data/model/model.py
@@ -2,4 +2,4 @@
 
 
 from . import common  # noqa analysis:ignore
-from .cotisations_sociales import travail  # noqa analysis:ignore
+from .cotisations_sociales import survey_variables # noqa analysis:ignore
