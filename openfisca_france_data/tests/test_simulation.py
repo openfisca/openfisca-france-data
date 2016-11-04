2401ab2599313e526369240f4adeca2a9173a7c1
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2016-11-04 19:43:52 +0100

    Clean

--- openfisca_france_data/tests/test_simulation.py
+++ openfisca_france_data/tests/test_simulation.py
@@ -37,7 +37,6 @@ def test_erfs_fpr_survey_simulation(year = 2012):
 
 
 def test_erfs_survey_simulation(year = 2009):
-
     survey_scenario = ErfsSurveyScenario.create(year = year)
     data_frame_by_entity_key_plural = survey_scenario.create_data_frame_by_entity_key_plural(
         variables = [
