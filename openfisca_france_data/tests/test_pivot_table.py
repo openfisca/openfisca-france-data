8e97cec8c0f5faae30cf9af510b8d3ee777cdaa6
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2016-11-15 16:17:14 +0100

    Improve tests

--- openfisca_france_data/tests/test_pivot_table.py
+++ openfisca_france_data/tests/test_pivot_table.py
@@ -2,10 +2,11 @@
 
 
 from openfisca_france_data.erfs.scenario import ErfsSurveyScenario
+from openfisca_france_data.erfs_fpr.scenario import ErfsFprSurveyScenario
 
 
-def test_pivot_table_1d_mean(year = 2009):
-    survey_scenario = ErfsSurveyScenario.create(year = year)
+def test_pivot_table_1d_mean():
+    survey_scenario = get_survey_scenario(kind = 'erfs_fpr')
     pivot_table = survey_scenario.compute_pivot_table(
         columns = ['decile_rfr'],
         values = ['irpp']
@@ -13,8 +14,8 @@ def test_pivot_table_1d_mean(year = 2009):
     return pivot_table
 
 
-def test_pivot_table_1d_sum(year = 2009):
-    survey_scenario = ErfsSurveyScenario.create(year = year)
+def test_pivot_table_1d_sum():
+    survey_scenario = get_survey_scenario(kind = 'erfs_fpr')
     pivot_table = survey_scenario.compute_pivot_table(
         aggfunc = 'sum',
         columns = ['decile_rfr'],
@@ -23,8 +24,8 @@ def test_pivot_table_1d_sum(year = 2009):
     return pivot_table
 
 
-def test_pivot_table_1d_count(year = 2009):
-    survey_scenario = ErfsSurveyScenario.create(year = year)
+def test_pivot_table_1d_count():
+    survey_scenario = get_survey_scenario(kind = 'erfs_fpr')
     pivot_table = survey_scenario.compute_pivot_table(
         aggfunc = 'count',
         columns = ['decile_rfr'],
@@ -33,14 +34,25 @@ def test_pivot_table_1d_count(year = 2009):
     return pivot_table
 
 
-def test_pivot_table_2d(year = 2009):
-    survey_scenario = ErfsSurveyScenario.create(year = year)
+def test_pivot_table_2d():
+    survey_scenario = get_survey_scenario(kind = 'erfs_fpr')
     pivot_table = survey_scenario.compute_pivot_table(
         columns = ['decile_rfr'],
         index = ['nbptr'],
         values = ['irpp']
         )
     return pivot_table
+
+
+def get_survey_scenario(kind = 'erfs_fpr'):
+    if kind == 'erfs_fpr':
+        year = 2012
+        SurveyScenario = ErfsFprSurveyScenario
+    else:
+        year = 2009
+        SurveyScenario = ErfsSurveyScenario
+
+    return SurveyScenario.create(year = year)
 
 
 if __name__ == '__main__':
@@ -50,5 +62,7 @@ if __name__ == '__main__':
     import sys
     logging.basicConfig(level = logging.INFO, stream = sys.stdout)
     start = time.time()
-    pivot_table = test_pivot_table_1d_sum(year = 2009)
+    print(test_pivot_table_1d_sum())
+    print(test_pivot_table_1d_mean())
+    print(test_pivot_table_2d())
     print(time.time() - start)
