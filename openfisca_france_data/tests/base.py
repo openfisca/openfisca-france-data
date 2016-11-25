c970cab38d846c4774aee52e52c23ed2452af96a
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2016-11-25 18:43:56 +0100

    Remove unused and buggy import

--- openfisca_france_data/tests/base.py
+++ openfisca_france_data/tests/base.py
@@ -5,13 +5,12 @@ from openfisca_core.tools import assert_near
 
 
 from openfisca_france.tests.base import get_cached_composed_reform, get_cached_reform
-from .. import france_data_tax_benefit_system, FranceDataTaxBenefitSystem
+from .. import france_data_tax_benefit_system
 
 
 __all__ = [
     'assert_near',
     'france_data_tax_benefit_system',
-    'FranceDataTaxBenefitSystem',
     'get_cached_composed_reform',
     'get_cached_reform',
     ]
