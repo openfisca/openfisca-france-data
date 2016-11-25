a0d7f8a8256054ea86dccbf3b525fc201294a9f2
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2016-11-25 18:34:43 +0100

    Remove tagging from travis

--- travis-release-tag.sh
+++ travis-release-tag.sh
@@ -11,4 +11,4 @@ then
 	ssh-add ./openfisca_bot
 fi
 git tag $version
-git push git@github.com:openfisca/openfisca-france.git --tags
+git push git@github.com:openfisca/openfisca-france-data.git --tags
