5bcc9d9083037437147223de94b9a5878d201b8d
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2018-03-27 11:50:43 +0200

    incemtal fix of get_survey_scenario

--- openfisca_france_data/erfs_fpr/get_survey_scenario.py
+++ openfisca_france_data/erfs_fpr/get_survey_scenario.py
@@ -12,6 +12,8 @@ from openfisca_france_data.tests import base as base_survey
 
 
 from openfisca_france_data.erfs_fpr.scenario import ErfsFprSurveyScenario
+
+
 def get_survey_scenario(year = 2012, rebuild_input_data = False, reform_key = None):
     tax_benefit_system = base_survey.get_cached_reform(
         reform_key = reform_key,
@@ -21,6 +23,8 @@ def get_survey_scenario(year = 2012, rebuild_input_data = False, reform_key = No
         tax_benefit_system = tax_benefit_system,
         baseline_tax_benefit_system = tax_benefit_system,
         year = year,
+        )
+    survey_scenario.init_from_survey_tables(
         rebuild_input_data = rebuild_input_data,
         )
     return survey_scenario
