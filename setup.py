7e4e26a8b94f9aeec6868a6f16f6996011c0faec
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2019-02-20 09:59:17 +0100

    Bump version to 0.7.8

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ doc_lines = __doc__.split('\n')
 
 setup(
     name = 'OpenFisca-France-Data',
-    version = '0.7.7',
+    version = '0.7.8',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [classifier for classifier in classifiers.split('\n') if classifier],
