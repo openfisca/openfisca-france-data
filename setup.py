38b459ffe84fbd0b38e2879dda94ece09176c77d
sandcha <sandcha@users.noreply.github.com>
2019-10-29 15:39:13 +0100

    Update module description in setup file

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('LICENSE') as file:
 setup(
     name = "OpenFisca-France-Data",
     version = "0.13.0",
-    description = "OpenFisca-France module to work with French survey data",
+    description = "OpenFisca-France-Data module to work with French survey data",
     long_description = long_description,
     author = "OpenFisca Team",
     author_email = "contact@openfisca.fr",
