ef8e18eefe6965466add4c90573def8fae8492dc
bchoho <bilal.choho@gmail.com>
2021-06-18 14:12:52 +0200

    encadr absent before 2006

--- openfisca_france_data/erfs_fpr/input_data_builder/step_01_preprocessing.py
+++ openfisca_france_data/erfs_fpr/input_data_builder/step_01_preprocessing.py
@@ -75,7 +75,7 @@ Il y a {} individus dans eec_individu
         agepr,
         cohab,
         'contra',
-        'encadr',
+        #'encadr',
         'forter',
         lien,
         'mrec',
@@ -90,7 +90,8 @@ Il y a {} individus dans eec_individu
         'titc',
         txtppb,
         ]
-         + (["noiper"] if "noiper" in individus.columns else []))
+         + (["noiper"] if "noiper" in individus.columns else [])
+         + (["encadr"] if "encadr" in individus.columns else []))
 
     for var in var_list:
         individus[var]=individus[var].fillna(0)
