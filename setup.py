bd57d0c2e2f7513d89d0e132a8d013dba2e969c4
bchoho <bilal.choho@gmail.com>
2021-08-04 17:32:07 +0200

    Version 64.0.1 of openfisca france

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
         "click >= 7.1.2, < 8.0.0",
         "matplotlib >= 3.1.1, < 4.0.0",
         "multipledispatch >= 0.6.0, < 1.0.0",
-        "openFisca-france >= 59.0.0, < 60.0.0",
+        "openFisca-france >= 64.0.1, < 65.0.0",
         "openFisca-survey-manager >= 0.44.2, < 1.0.0",
         "wquantiles >= 0.3.0, < 1.0.0",  # To compute weighted quantiles
         ],
