2b81d873ca28e56629c0b7ff3faae0ca98795e6c
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2021-08-23 22:45:57 +0200

    Fix parenthesis

--- openfisca_france_data/smic.py
+++ openfisca_france_data/smic.py
@@ -101,7 +101,10 @@ smic_annuel_imposable_by_year = dict([
 
 
 smic_horaire_brut_by_year = dict([
-    (year, openfisca_france_tax_benefit_system.get_parameters_at_instant(instant = periods.period(year).marche_travail.salaire_minimum.smic_h_b)
+    (
+        year,
+        openfisca_france_tax_benefit_system.get_parameters_at_instant(instant = periods.period(year).marche_travail.salaire_minimum.smic_h_b)
+        )
     for year in range(2002, 2021)
     ])
 
