08fc2bb6b2f93bd84c9ed6656a619223655e15b8
@ChloeLallemand <chloe.lallemand@ipp.eu>
2021-08-25 12:02:57 +0200

    Complète le test_get_baremes_salaries

--- tests/test_get_baremes_salarie.py
+++ tests/test_get_baremes_salarie.py
@@ -11,6 +11,22 @@ parameters = tax_benefit_system.parameters
 
 target_by_period_by_categorie_salarie = {
     'prive_non_cadre': {
+        2016: [
+            'agff',
+            'arrco',
+            'asf',
+            'chomage',
+            'vieillesse_deplafonnee',
+            'vieillesse_plafonnee',
+            ],
+        2017: [
+            'agff',
+            'arrco',
+            'asf',
+            'chomage',
+            'vieillesse_deplafonnee',
+            'vieillesse_plafonnee',
+            ],
         2018: [
             'agff',
             'arrco',
@@ -29,6 +45,146 @@ target_by_period_by_categorie_salarie = {
             'vieillesse_plafonnee',
             ],
         },
+    'prive_cadre': {
+        2016: [
+            'agff',
+            'arrco',
+            'agirc',
+            'apec',
+            'asf',
+            'chomage',
+            'vieillesse_deplafonnee',
+            'vieillesse_plafonnee',
+            ],
+        2017: [
+            'agff',
+            'arrco',
+            'agirc',
+            'apec',
+            'asf',
+            'chomage',
+            'vieillesse_deplafonnee',
+            'vieillesse_plafonnee',
+            ],
+        2018: [
+            'agff',
+            'arrco',
+            'agirc',
+            'apec',
+            'asf',
+            'chomage',
+            'vieillesse_deplafonnee',
+            'vieillesse_plafonnee',
+            ],
+        2019: [
+            'agirc_arrco',
+            'asf',
+            'apec',
+            'ceg',
+            'cet2019',
+            'chomage',
+            'vieillesse_deplafonnee',
+            'vieillesse_plafonnee',
+            ],
+        },
+    'public_non_titulaire': {
+        2016: [
+            'excep_solidarite',
+            'ircantec',
+            'maladie_alsace_moselle',
+            'maladie',
+            'vieillesse_deplafonnee',
+            'vieillesse_plafonnee',
+            ],
+        2017: [
+            'excep_solidarite',
+            'ircantec',
+            'maladie_alsace_moselle',
+            'maladie',
+            'vieillesse_deplafonnee',
+            'vieillesse_plafonnee',
+            ],
+        2018: [
+            'ircantec',
+            'maladie_alsace_moselle',
+            'vieillesse_deplafonnee',
+            'vieillesse_plafonnee',
+            ],
+        2019: [
+            'ircantec',
+            'maladie_alsace_moselle',
+            'vieillesse_deplafonnee',
+            'vieillesse_plafonnee',
+            ],
+        },    
+    "public_titulaire_etat": {
+        2016: [
+            "excep_solidarite",
+            "pension",
+            "rafp",
+            ],
+        2017: [
+            "excep_solidarite",
+            "pension",
+            "rafp",
+            ],
+        2018: [
+            "pension",
+            "rafp",
+            ],
+        2019: [
+            "pension",
+            "rafp",
+            ],
+    "public_titulaire_hospitaliere": {
+        2016: [
+            "cnracl_s_ti",
+            "cnracl_s_nbi",
+            "excep_solidarite",
+            "rafp",
+            ],
+        2017: [
+            "cnracl_s_ti",
+            "cnracl_s_nbi",
+            "excep_solidarite",
+            "rafp",
+            ],
+        2018: [
+            "cnracl_s_ti",
+            "cnracl_s_nbi",
+            "rafp",
+            ],
+        2019: [
+            "cnracl_s_ti",
+            "cnracl_s_nbi",
+            "rafp",
+            ],
+    "public_titulaire_territoriale": {
+        2016: [
+            "cnracl_s_ti",
+            "cnracl_s_nbi",
+            "excep_solidarite",
+            "rafp",
+            ],
+        2017: [
+            "cnracl_s_ti",
+            "cnracl_s_nbi",
+            "excep_solidarite",
+            "rafp",
+            ],
+        2018: [
+            "cnracl_s_ti",
+            "cnracl_s_nbi",
+            "rafp",
+            ],
+        2019: [
+            "cnracl_s_ti",
+            "cnracl_s_nbi",
+            "rafp",
+            ],
+        },
+    
+
     }
 
 
