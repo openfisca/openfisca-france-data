effd1c6f6e93e099fe6b9558dd3ffb8594d70cc6
Lukas Puschnig <lukas.puschnig@ipp.eu>
2022-06-03 09:52:08 +0200

    [WIP] Begins big overhaul of SMIC calculations (on hold, waiting for OFF update)

--- openfisca_france_data/smic.py
+++ openfisca_france_data/smic.py
@@ -28,9 +28,12 @@ for year in range(2002, 2021):
 # partir du chiffre de 2010. Les résultats pour les années après 2010 sont les
 # mêmes à un euro près. 1996-1999 ont la meeeeeême valeur que 2000.
 smic_annuel_net_by_year = {
-    2020: 12 * 1200.0,
-    2019: 12 * 1200.0,
-    2018: 9 * 1173.60 + 3 * 1187.83,  # Baisse de la cotisaation chômage en cours d'annnée
+    2022: 4 * 1269.02 + 8 * 1302.64, # latest value as of May, assuming no change over the year to come
+    2021: 9 * 1230.6 + 3 * 1258.22,
+
+    2020: 12 * 1218.6,
+    2019: 12 * 1204.19,
+    2018: 9 * 1173.60 + 3 * 1187.83,  # Baisse de la cotisation chômage en cours d'annnée
     2017: 12 * 1151.50,
     2016: 12 * 1141.61,
     2015: 12 * 1135.99,
@@ -38,76 +41,131 @@ smic_annuel_net_by_year = {
     2013: 12 * 1120.43,
     2012: 2 * 1116.87 + 4 * 1118.29 + 6 * 1096.88,
     2011: 11 * 1072.07 + 1094.71,
+
     2010: 12 * 1056.24,
-    2009: 12 * 1044.91,
-    2008: 12 * 1026.01,
-    2007: 12 * 995.76,
-    2006: 12 * 970.81,
-    2005: 12 * 933.01,
-    2004: 12 * 885.37,
-    2003: 12 * 838.50,
-    2002: 12 * 810.52,
-    2001: 12 * 784.82,
-    2000: 12 * 756.08,
-    1999: 12 * 756.08,
-    1998: 12 * 756.08,
-    1997: 12 * 756.08,
-    1996: 12 * 756.08,
+    2009: 6 * 1050.63 + 6 * 1037.53,
+    2008: 6 * 1037.53 + 2 * 1028 + 4 * 1005.36,
+    2007: 6 * 1005.36 + 6 * 985.11,
+    2006: 6 * 984.61 + 6 * 956.04,
+    2005: 12 * 933,
+    2004: 12 * 880,
+    2003: 12 * 838,
+    2002: 12 * 810,
+    2001: 12 * 785,
+    2000: 12 * 756, # 2000 onwards: based on 151.67 working hours
+
+    1999: 12 * 823,
+    1998: 12 * 813,
+    1997: 12 * 784,
+    1996: 12 * 759,
+    1995: 12 * 739,
+    1994: 12 * 718,
+    1993: 12 * 709,
+    1992: 12 * 700,
+    1991: 12 * 679,
+    1990: 12 * 651,
+
+    1989: 12 * 624,
+    1988: 12 * 606,
+    1987: 12 * 594,
+    1986: 12 * 575,
+    1985: 12 * 556,
+    1984: 12 * 516,
+    1983: 12 * 478,
+    1982: 12 * 429, # 1982 onwards: based on 169 working hours
+    1981: 12 * 378,
+    1980: 12 * 317,
+
+    1979: 12 * 277,
+    1978: 12 * 252,
+    1977: 12 * 223,
+    1976: 12 * 199,
+    1975: 12 * 175,
+    1974: 12 * 147,
+    1973: 12 * 119,
+    1972: 12 * 102,
+    1971: 12 * 91,
+    1970: 12 * 83,
+
+    1969: 12 * 76,
+    1968: 12 * 65,
+    1967: 12 * 51,
+    1966: 12 * 50,
+    1965: 12 * 48,
+    1964: 12 * 46,
+    1963: 12 * 46,
+    1962: 12 * 42,
+    1961: 12 * 40,
+    1960: 12 * 40,
+
+    1959: 12 * 39,
+    1958: 12 * 36,
+    1957: 12 * 33,
+    1956: 12 * 31,
+    1955: 12 * 31,
+    1954: 12 * 29,
+    1953: 12 * 24,
+    1952: 12 * 24,
+    1951: 12 * 23,
+    1950: 12 * 20, # 1950 onwards: based on 173.3 working hours
     }
 
-abattement_by_year = {
-    2020: .0175,
-    2019: .0175,
-    2018: .0175,
-    2017: .0175,
-    2016: .0175,
-    2015: .0175,
-    2014: .0175,
-    2013: .0175,
-    2012: .0175,
-    2011: .03,
-    2010: .03,
-    2009: .03,
-    2008: .03,
-    2007: .03,
-    2006: .03,
-    2005: .03,
-    2004: .03,
-    2003: .03,
-    2002: .03,
-    2001: .03,
-    2000: .03,
-    1999: .03,
-    1998: .03,
-    1997: .03,
-    1996: .03,
-    }
+# get availability of parameters
+t = openfisca_france_tax_benefit_system.parameters.prelevements_sociaux.contributions.csg.abattement.sous_4pss
+t3 = t.values_list
+sd = t3[t3.__len__() - 1]
+sd.instant_str
 
-def smic_annuel_imposable_from_net(year):
+# check coverage
+start_year = min(smic_annuel_net_by_year.keys())
+end_year = max(smic_annuel_net_by_year.keys())
+
+smic_horaire_brut = dict()
+for year in range(start_year, end_year+1):
+    try:
+        # this collects the data from openfisca-france/openfisca_france/parameters/marche_travail/salaire_minimum/smic/smic_b_horaire.yaml ?
+        smic_horaire_brut[year] = openfisca_france_tax_benefit_system.get_parameters_at_instant(instant = periods.period(year).start).cotsoc.gen.smic_h_b
+    except:
+        continue
+
+# recheck coverage for gross hourly SMIC availability
+start_year = min(smic_horaire_brut.keys())
+end_year = max(smic_horaire_brut.keys())
+
+def smic_annuel_imposable_from_net(year, smic_hor_brut):
+    params = openfisca_france_tax_benefit_system.get_parameters_at_instant(instant = periods.period(year).start)
     smic_net = smic_annuel_net_by_year[year]
-    smic_brut = smic_horaire_brut[year] * 35 * 52
-    smic_imposable = (
-        smic_net
-        + (.024 + 0.005) * (1 - abattement_by_year[year]) * smic_brut
-        )
+    working_hours = params.cotsoc.gen.nb_heure_travail_mensuel
+    smic_brut = smic_hor_brut * working_hours * 12
+    taux_csg = params.prelevements_sociaux.contributions.csg.activite.imposable.taux
+    taux_crds = params.prelevements_sociaux.contributions.crds.activite.taux
+    abatt_sous_4pss = params.prelevements_sociaux.contributions.csg.abattement.sous_4pss
+    abatt_dessus_4pss = params.prelevements_sociaux.contributions.csg.abattement.au_dessus_de_4_pss
+    pss = params.prelevements_sociaux.pss.plafond_de_la_securite_sociale_annuel
+
+    # precise formula is kinda unnecessary, since SMIC won't ever be beyond 4 PSS. but still, for the heck of it.
+    base_csg_crds = (1 - abatt_sous_4pss) * min(smic_brut, 4 * pss) * (smic_brut > 4 * pss) * (1 - abatt_dessus_4pss) * (smic_brut - 4 * pss)
+
+    # final result, add CSG and CRDS to SMIC net
+    smic_imposable = (smic_net + (taux_csg + taux_crds) * base_csg_crds)
+
     return smic_imposable
 
 
 smic_annuel_imposable_by_year = dict([
-    (year, smic_annuel_imposable_from_net(year))
-    for year in range(2002, 2021)
+    (year, smic_annuel_imposable_from_net(year, smic_horaire_brut[year]))
+    for year in range(start_year, end_year)
     ])
 
 
 smic_horaire_brut_by_year = dict([
-    (
-        year,
-        openfisca_france_tax_benefit_system.parameters(year).marche_travail.salaire_minimum.smic.smic_b_horaire
-        )
-    for year in range(2002, 2021)
+    (year, openfisca_france_tax_benefit_system.get_parameters_at_instant(instant = periods.period(year).start).cotsoc.gen.smic_h_b)
+    for year in range(start_year, end_year)
     ])
 
+
 smic_annuel_brut_by_year = dict([
-    (year, value * 35 * 52)
-    for year, value in smic_horaire_brut_by_year.items()
+    (year,
+    smic_horaire_brut_by_year[year] * openfisca_france_tax_benefit_system.get_parameters_at_instant(instant = periods.period(year).start).cotsoc.gen.nb_heure_travail_mensuel * 12)
+    for year in range(start_year, end_year)
     ])
