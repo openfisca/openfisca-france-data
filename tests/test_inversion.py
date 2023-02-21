e3ea4f6110fbe47ee05bf8ba7dfaa55262ca533f
paul <paul.dutronc@ipp.eu>
2023-02-21 14:40:43 +0100

    first test inversion salaires

--- tests/test_inversion.py
+++ tests/test_inversion.py
@@ -3,7 +3,9 @@ from yaml import load, SafeLoader
 import os
 import re
 
-from openfisca_core.periods import *
+from openfisca_core.periods import period as p
+from openfisca_core.taxscales import MarginalRateTaxScale, combine_tax_scales
+from openfisca_core.formula_helpers import switch
 
 from openfisca_france import FranceTaxBenefitSystem
 
@@ -17,23 +19,25 @@ margin = 1
 tax_benefit_system = FranceTaxBenefitSystem()
 scenario = tax_benefit_system.new_scenario()
 
+# Revenus de remplacement
+
 ## First part : upwards (start from *_taxable, inverse to *_gross)
 
-# Data creation
+### Data creation
 
-cd = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-path = os.path.join(cd, "tests", "inversion", "remplacement_2021.yaml")
+cd = os.path.dirname(__file__)
+path = os.path.join(cd, "inversion", "remplacement_2021.yaml")
 year = re.match(".*([0-9]{4}).yaml", path).group(1)
 
 with open(path) as yaml:
     individus = pd.DataFrame.from_dict(load(yaml, Loader=SafeLoader))
 
-# Inverse incomes from net to gross : the tested functions
+### Inverse incomes from net to gross : the tested functions
 
 create_taux_csg_remplacement(individus, period(year), tax_benefit_system)
 create_revenus_remplacement_bruts(individus, period(year), tax_benefit_system)
 
-# Test against chomage_brut_test
+### Test against chomage_brut_test
 
 fails_chomage = [i for i in individus.index if abs(individus.loc[i]["chomage_brut"]-individus.loc[i]["chomage_brut_test"])>=margin]
 fails_retraite = [i for i in individus.index if abs(individus.loc[i]["retraite_brute"]-individus.loc[i]["retraite_brute_test"])>=margin]
@@ -45,14 +49,14 @@ message = "".join(
 
 assert len(fails_chomage) + len(fails_retraite) ==0, "Some tests have failed.\n" + message
 
-## Second part : downwards (start from gross obtained from inversion, goes back to taxable)
+# ## Second part : downwards (start from gross obtained from inversion, goes back to taxable)
 
-# Initialize the survey scenario with the gross (inverted)
+# ### Initialize the survey scenario with the gross (inverted)
 
 # init_single_entity(scenario, init_data)
 # simulation = scenario.new_simulation()
 
-# # Computes *_taxable back from inverted *_gross
+# ### Computes *_taxable back from inverted *_gross
 
 # simulation.calculate('chomage_imposable', '2021-01') == 1000
 # simulation.calculate('csg_deductible_chomage', '2021-01') == 0
