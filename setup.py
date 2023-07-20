f4edad5e1c3fdbcfd6b0c781811bd5b50c48b262
cgl <lallemand.chloe@gmail.com>
2023-07-20 18:24:01 +0200

    bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as file:
 
 setup(
     name = "OpenFisca-France-Data",
-    version = "1.3.0",
+    version = "1.3.1",
     description = "OpenFisca-France-Data module to work with French survey data",
     long_description = long_description,
     long_description_content_type="text/markdown",
