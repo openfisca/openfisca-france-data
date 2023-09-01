d6969732a4c5d9c77771c72aff1f9537bb796761
cgl <lallemand.chloe@gmail.com>
2023-09-01 17:48:19 +0200

    Ajoute des variables de csg dans model/common

--- setup.py
+++ setup.py
@@ -28,7 +28,9 @@ setup(
     package_data = {
         'openfisca_france_data': ['assets/aggregats/taxipp/agregats_tests_taxipp_2_0.xlsx',
                                   'assets/aggregats/ines/ines_2019.json',
-                                  'reforms/old_openfisca_france_reforms/parameters/*.yaml'],
+                                  'reforms/old_openfisca_france_reforms/parameters/*.yaml',
+                                  'assets/aggregats/ines/ines_2020.json',
+                                  'assets/aggregats/france_entiere/*.json'],
         },
     entry_points = {
         'console_scripts': [
