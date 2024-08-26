1cdb4f30d9849c80481cc94f3eb76c2e51a71cb1
Duchesne Sylvain <sylvain.duchesne@ipp.eu>
2024-08-26 09:37:29 +0200

    Bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as file:
 
 setup(
     name = "OpenFisca-France-Data",
-    version = "3.6.0",
+    version = "3.6.1",
     description = "OpenFisca-France-Data module to work with French survey data",
     long_description = long_description,
     long_description_content_type="text/markdown",
