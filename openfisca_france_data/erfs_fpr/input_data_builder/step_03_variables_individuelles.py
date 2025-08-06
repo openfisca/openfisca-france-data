4caeb5173ec59aa1a302c8e7c415ca3761039a0c
chloelallemand <55277136+clallemand@users.noreply.github.com>
2025-08-06 14:07:19 +0200

    Update openfisca_france_data/erfs_fpr/input_data_builder/step_03_variables_individuelles.py
    Co-authored-by: Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>

--- openfisca_france_data/erfs_fpr/input_data_builder/step_03_variables_individuelles.py
+++ openfisca_france_data/erfs_fpr/input_data_builder/step_03_variables_individuelles.py
@@ -1339,7 +1339,7 @@ def create_variables_handicap(individus, year):
     # 3 : Non
     # 9 : Non réponse 
     if year >= 2022:
-        individus['handicap'] = np.where(individus.admhandr == 1,True,False)
+        individus['handicap'] = np.where(individus.admhandr == 1, True, False)
         individus['taux_incapacite'] = np.where(individus.admhandr == 1,0.8,0)
 
 def create_revenus_remplacement_bruts(individus):
