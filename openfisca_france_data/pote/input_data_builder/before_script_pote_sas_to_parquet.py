a9c2fb889cdcda4add30381311f0458c1129834b
cgl <cgl>
2026-01-05 15:23:50 +0100

    Met à jour le script de transformation de POTE SAS to parquet

--- openfisca_france_data/pote/input_data_builder/before_script_pote_sas_to_parquet.py
+++ openfisca_france_data/pote/input_data_builder/before_script_pote_sas_to_parquet.py
@@ -1,62 +1,59 @@
 """
-Ce script converti le fichier SAS de POTE en format parquet dans deux structures distinctes : 
-- Environs 3 000 fichiers parquets qui contiennent chacun toutes les colonnes mais seulement 100 000 lignes
-- Un fichier par colonne avec toutes les lignes.
-- Un fichier Excel de statistiques descriptives des colonnes.
+Ce script transforme la base de données POTE qui est au format SAS en format parquet, plus facile à manier
 """
-import pandas as pd
-import json
+
 import gc
-import glob
+import json
 import os
-import pyarrow.parquet as pq
-import pyarrow.compute as pc
+import pyreadstat
 
-def pote_sas_to_parquet(year, sas_pote_path, chunks_path, columns_path):
+from openfisca_france_data.utils import build_cerfa_fields_by_variable
 
-    sas_file = f"{os.path.join(sas_pote_path,year)},pote_diff_{year}.sas7bdat"
+def pote_sas_to_parquet(year, sas_pote_path, parquet_path, create_labels = False, ncols = 70):
+    """
+    Docstring for pote_sas_to_parquet
+    
+    :param year: Millésime de Pote
+    :param sas_pote_path: Chemin de la base pote en SAS
+    :param parquet_path: Chemin où enregistrer la base pote en parquet créée
+    :param create_labels: bool, Si True recrée la liste des variables dans la base SAS
+    :param ncols: Nombre max de colonnes à charger en même temps
+    """
+    labels_path = os.path.join(parquet_path, "metadata")
+    if create_labels:
+        if not os.path.exists(labels_path):
+            os.makedirs(labels_path)
+        sas_file = f"{os.path.join(sas_pote_path,year)},pote_diff_{year}.sas7bdat"
+        metadata = pyreadstat.read.sas7bdat(filename_path = sas_file)
+        col_labels = metadata[1].column_names_to_labels
+        with open(os.path.join(labels_path, "labels.json"), "w", newline='',encoding='utf-8') as f:
+            json.dump(col_labels, f, ensure_ascii=False, indent=4)
+    else:
+        with open(os.path.join(labels_path, "labels.json"),"r",encoding='utf-8') as f:
+            col_labels=json.load(f)
+    
+    cerfa = build_cerfa_fields_by_variable(year = year)
+    openfisca_cerfa_list = set()
+    for k,v in cerfa.items():
+        openfisca_cerfa_list = (openfisca_cerfa_list | set([vv[1:] for vv in v]))
 
-    chunks_path = os.path.join(chunks_path, year)
-    columns_path = os.path.join(columns_path, year)
-    taille_chunk = 100_000
+    # on ajoute les cases qui ne sont pas renseignées cerfa dans openfisca-france
+    list_col += ["aged", "agec", "mat", "f", "h", "r", "j", "n", "g", "i", "nbfoy", "dnpa4c", "r", "zl", "zn", "zp", "zf", "zw", "zs", "zg", "zt", "zz", "iddep"]
 
-    dfi = pd.read_sas(
-        sas_file, chunksize=taille_chunk, encoding="iso8859-15", iterator=True
-    )
+    for var in list_col:
+        assert col_labels.get(var, None) is not None, f"{var} not in col_labels"
+    
+    del cerfa
+    del openfisca_cerfa_list
+    del col_labels
 
-    # 1) on fait des chunks au format parquet de la table complète en SAS.
-    i = 0
-    for chunk in dfi:
-        chunk.columns = [c.lower() for c in chunk.columns.to_list()]
-        chunk.drop(["fip18_c"], axis=1, inplace=True)
-        chunk.to_parquet(f"{chunks_path}/pote_{i}.parquet")
-        del chunk
+    for i in range(1,len(list_col)//ncols + 1):
+        print(f"Round {i} / {len(list_col)//ncols + 1}")
+        table, metadata = pyreadstat.read_sas7bdat(filename_path = sas_pote_path, usecols=list_col[i*ncols:(i+1)*ncols])
+        table.to_parquet(os.path.join(parquet_path,f"pote_2023_{i}.parquet"))
+        with open(os.path.join(labels_path, f"labels_pote_{i}.json"),"w",newline='',encoding='utf-8') as f:
+            json.dump(metadata.column_names_to_labels, f, ensure_ascii=False,indent=4)
+        del table
+        del metadata
         gc.collect()
 
-    # 2) On lit tous les chunks pour faire une table parquet par colonne
-    parquet_file = glob.glob(os.path.join(chunks_path, "*.parquet"))
-
-    dfv = pq.ParquetDataset(parquet_file)
-
-    column_names = list()
-    for col in dfv.schema:
-        column_names.append(col.name)
-
-    stats = dict()
-
-    for col in column_names:
-        print(col)
-        datas = dfv.read([col])
-        stats[col] = {
-            'nombre_na' : datas.column(col).null_count,
-            'dtype' : str(datas.column(col).type)
-        }
-        if datas.column(col).type in ("double", "integer"):
-            stats[col]['somme'] = pc.sum(datas.column(col)).as_py()
-        pq.write_table(datas,f"{columns_path}/pote_{col}.parquet")
-
-    with open(os.path.join(columns_path,"columns_stats_desc.json"), "w") as outfile:
-        json.dump(stats, outfile)
-
-    stats_corr = pd.DataFrame(stats)
-    stats_corr.transpose().to_excel(os.path.join(columns_path,"columns_stats_desc.xlsx"))
